6bbd8fda2303451be3261aa89727ac710f1522c7
palloncino <antonio.guiotto.dev@gmail.com>
2024-06-21 00:01:14 +0200

    attempt

--- init.js
+++ init.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function adjustWidth() {
         const appContainer = document.querySelector('.app-container');
         const header = document.querySelector('.header');
+        const scrollBar = document.querySelector('.custom-scrollbar');
         const viewportHeight = window.innerHeight;
         const viewportWidth = window.innerWidth;
         let computedWidth = (16 / 9) * viewportHeight;
@@ -12,8 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
             computedWidth = viewportWidth;
         }
 
+        const marginRight = (viewportWidth - computedWidth) / 2;
+
         appContainer.style.width = `${computedWidth}px`;
         header.style.width = `${computedWidth}px`;
+        scrollBar.style.marginRight = `calc(${marginRight}px + 1rem)`; // Adjust scrollbar margin-right
     }
 
     window.addEventListener('resize', adjustWidth);
